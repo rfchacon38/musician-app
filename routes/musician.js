88ae97815978eb22a3a087a9dafac72817ee56b3
Richard Chacon <rfchacon38@yahoo.com>
2019-08-22 13:24:08 -0700

    testing stuff

--- routes/musician.js
+++ routes/musician.js
@@ -6,7 +6,7 @@ const schema = require('../store/schema');
 
 // healthcheck
 router.get('/health', (req, res) => {
-  res.status('200').send("Status: cool!");
+  res.status('200').send("Status: cool1!");
 });
 
 // retrieve all musicians from data store
